870063c0a9e1cdcf1df4abd17310d3a93778c31f
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-11-21 08:57:56 +0000

    Update glossary.cs

--- glossary/glossary.cs
+++ glossary/glossary.cs
@@ -5,3 +5,10 @@
 // - Presentation (Example: PostView)
 // - Business Logic / Domain (Example: Post)
 // - Data Access / Persistence (Example: PostRepository)
+
+
+
+// Anotomy of a Class
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+// Data (Represented by fields)
+// Behavior (Represented by methods / functions)
