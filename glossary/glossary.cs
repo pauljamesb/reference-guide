a57ae10e97bada8f794c754bcb1bd71b741b0b93
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-11-21 09:18:05 +0000

    Update glossary.cs

--- glossary/glossary.cs
+++ glossary/glossary.cs
@@ -2,28 +2,36 @@
 // +++++++++++++++++++++++++++++++++++++++++++++++++
 
 // A real world application may consist of:
-// - Presentation (Example: PostView)
-// - Business Logic / Domain (Example: Post)
-// - Data Access / Persistence (Example: PostRepository)
+// - Presentation (Example: PostView).
+// - Business Logic / Domain (Example: Post).
+// - Data Access / Persistence (Example: PostRepository).
 
 
 
 // Anatomy of a Class
 // +++++++++++++++++++++++++++++++++++++++++++++++++
-// Data (Represented by fields)
-// Behavior (Represented by methods / functions)
+// Data (Represented by fields).
+// Behavior (Represented by methods / functions).
 // Class members: 
-// Instance member - Accessible from an object
-// Static member - Accessible from the Class
+// Instance member - Accessible from an object.
+// Static member - Accessible from the Class.
+
+
+
+// Static member : Anatomy of a Class
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+// To represent concepts that are singleton.
+// For example "Console.WriteLine();"
+// Used where you only want one instance of the concept in the memory.
 
 
 
 // Object
 // +++++++++++++++++++++++++++++++++++++++++++++++++
-// Object is an instance of a class
+// Object is an instance of a class.
 
 
 // Naming conventions
 // +++++++++++++++++++++++++++++++++++++++++++++++++
-// For Classes use Pascal Case
-// Use Camel Case for the naming of methods 
+// For Classes use Pascal Case.
+// Use Camel Case for the naming of methods. 
