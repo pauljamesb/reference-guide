03ce1d23968ab4228d62329d43b75554f46a2141
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-11-21 09:27:42 +0000

    Update classes.cs

--- intermediate/classes.cs
+++ intermediate/classes.cs
@@ -1,2 +1,11 @@
 // Create an object from a string
 // +++++++++++++++++++++++++++++++++++++++++++++++++
+// Once object "Person" for example has already being created.
+
+public Person Parse(string str)
+{
+  var person = new Person();
+  person.Name = str;
+  
+  return person;
+}
