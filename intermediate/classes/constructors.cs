a17c1bf5ce4da143dcbc83d0abf839a58dff92ae
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-11-21 10:37:48 +0000

    Update constructors.cs

--- intermediate/classes/constructors.cs
+++ intermediate/classes/constructors.cs
@@ -16,3 +16,22 @@ public class Customer
 
 // With this values can be passed when the object is created.
 var customer = new Customer("John");
+
+
+// Example of Overloaded Constructors
+public class Customer 
+{
+  public int Id;
+  public string Name;
+
+  public Customer(int id)
+  {
+    this.Id = id;
+  }
+
+  public Customer(int id, string name)
+  {
+    this.Id = id;
+    this.Name = name;
+  }
+}
