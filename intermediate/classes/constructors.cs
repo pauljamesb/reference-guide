ce86e0545115d47af773c44d0bd3c467a2531f62
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-11-21 10:05:21 +0000

    Create constructors.cs

---
+++ intermediate/classes/constructors.cs
@@ -0,0 +1,15 @@
+// Constructors
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+// Basic example
+// "this" help to ensure the right assignments operator is used.
+
+public class Customer
+{
+  public string Name;
+  
+  public Customer(string name)
+  {
+    this.Name = name;
+  }
+}
