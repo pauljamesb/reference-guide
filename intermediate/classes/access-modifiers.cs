36d532859eaec40b0f2a0145a5e2785355197445
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-12-05 11:59:14 +0000

    Update access-modifiers.cs

--- intermediate/classes/access-modifiers.cs
+++ intermediate/classes/access-modifiers.cs
@@ -1,6 +1,19 @@
 // Access Modifiers
+// Used for security.
+
 // Public
 // Private
 // Protected
 // Internal
 // Protected Internal
+
+
+public class Customer 
+{
+  private string Name;
+}
+
+// This would not complie
+var john = new Customer();
+john.Name;
+
