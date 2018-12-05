b7cdf10f173940e18d67e08039ab292a7323d228
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-12-05 11:34:44 +0000

    Update fields.cs

--- intermediate/classes/fields.cs
+++ intermediate/classes/fields.cs
@@ -24,6 +24,7 @@ public class Customer
 
 // Read-only Fields
 // This ensures there would only be one list where orders would be kept.
+// If it got initialized else where the data would not be lost.
 Public class Customer
 {
     readonly List<Order> Orders = new List<Order>();
