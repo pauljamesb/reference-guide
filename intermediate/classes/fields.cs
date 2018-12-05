a6e5596dd63e39b05c8c4502cedc276c9ca6ae18
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-12-05 11:54:20 +0000

    Update fields.cs

--- intermediate/classes/fields.cs
+++ intermediate/classes/fields.cs
@@ -22,6 +22,8 @@ public class Customer
 }
 
 
+
+
 // Read-only Fields
 // This ensures there would only be one list where orders would be kept.
 // If it got initialized else where the data would not be lost.
@@ -29,3 +31,58 @@ Public class Customer
 {
     readonly List<Order> Orders = new List<Order>();
 }
+
+
+// Working example:
+using System;
+using System.Collections.Generic;
+
+namespace Fields
+{
+    
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            //..
+            Orders = new List<Order>();
+        }
+    }
+
+
+    public class Order 
+    {}
+    
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            // Pretend bug, initializes the list again
+            // The readonly modifier prevents this
+            // An error will show in Promote();
+            customer.Promote();
+
+            Console.WriteLine(customer.Orders.Count);
+        }
+    }
+}
+
